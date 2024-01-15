b84a127133a5bff36aad109efb14629fa0a74f42
Aiman <ahardao1001@g.educaand.es>
2024-01-15 10:14:14 +0100

    main hecho

---
+++ src/main/java/com/mycompany/tarea5daimanyrocio/main.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.tarea5daimanyrocio;
+
+/**
+ *
+ * @author aiman
+ */
+public class main {
+    public static void main(String[] args) {
+        Tarea5DAimanyRocio t1 = new Tarea5DAimanyRocio();
+        
+        t1.setNumero(1);
+        t1.setPagina(5);
+        
+        System.out.println(t1);
+    }
+}

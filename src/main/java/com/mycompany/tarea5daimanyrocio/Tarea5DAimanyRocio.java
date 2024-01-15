0910bd989efdab0c3e0eeb2325309ad91b3d9c52
ralcara908 <ralcara908@g.educaand.es>
2024-01-15 10:05:10 +0100

    “Añade atributos en clase modelo”.

---
+++ src/main/java/com/mycompany/tarea5daimanyrocio/Tarea5DAimanyRocio.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tarea5daimanyrocio;
+
+/**
+ *
+ * @author rocio
+ */
+public class Tarea5DAimanyRocio {
+    int numero;
+    int pagina;
+
+}
